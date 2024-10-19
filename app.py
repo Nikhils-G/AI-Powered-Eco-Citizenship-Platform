d5f1f69d4e2b9c9f90f5bdffb8782ea6e6bb0f59
Nikhil's <130584896+Nikhils-G@users.noreply.github.com>
2024-10-19 18:55:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 # Ensure the static folder exists
 os.makedirs(app.static_folder, exist_ok=True)
 
-# Load the model
+
 model_path = 'model.pkl'
 with open(model_path, 'rb') as f:
     model = pickle.load(f)
@@ -20,17 +20,17 @@ def index():
 def serve_static(path):
     return send_from_directory(app.static_folder, path)
 
-# Route for making predictions
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Assume the input is JSON data with features for prediction
-    data = request.json
-    features = [data['dashboard'], data['Activities'], data['Community'],data['Rewards'], data['Insights'], data['Challenges']]  # Adjust based on your model's input
     
-    # Make a prediction using the model
+    data = request.json
+    features = [data['dashboard'], data['Activities'], data['Community'],data['Rewards'], data['Insights'], data['Challenges']]  
+    
+    
     prediction = model.predict([features])
     
-    # Return the prediction result as a JSON response
+    
     return jsonify({'prediction': prediction[0]})
 
 if __name__ == '__main__':
